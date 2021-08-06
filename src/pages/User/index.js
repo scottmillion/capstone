7c4ea70a5a5e02d156c54f85afea0f75f4a240c1
Scott Million <scottmillion@gmail.com>
2021-08-06 08:57:32 +0700

    first commit

---
+++ src/pages/User/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class User extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>This is the user page</h1>
+      </div>
+    )
+  }
+}
+
+export default User;
